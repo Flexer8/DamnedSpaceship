a391211107cf2666c58a57a2e2ffeedf2292554b
NickFlexer <nickflexer@gmail.com>
2019-03-03 11:44:43 +0300

    Добавлена пустая структура для состояния геймплея

---
+++ src/states/gameplay/st_gameplay.lua
@@ -0,0 +1,19 @@
+---
+-- таблица с состоянием Gameplay
+
+
+st_gameplay = {}
+
+function st_gameplay:init()
+	-- body...
+end
+
+function st_gameplay:update(dt)
+    -- body...
+end
+
+function st_gameplay:draw()
+    -- body...
+end
+
+return st_gameplay
